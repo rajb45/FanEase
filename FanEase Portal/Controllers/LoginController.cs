ec79e4705b99f6d6ee5d34d3a623cd2d94e0dac3
rajb45 <rajmbhosale3@gmail.com>
2021-10-19 12:48:11 +0530

    Initial Commit

---
+++ FanEase Portal/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using FanEase.Model.LoginModel;
+using FanEase_Portal.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanEase_Portal.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogin _logInUserService;
+        public LoginController(ILogin logInUserService)
+        {
+            _logInUserService = logInUserService;
+        }
+        public IActionResult Index()
+        {
+            return View(new Login());
+        }
+        [HttpPost]
+        public IActionResult Index(Login model)
+        {
+            model.Username = "Shankar";
+            model.Password = "Shankar";
+            _logInUserService.GetLoginUserDetail(model.Username, model.Password);
+            return View(model);
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
